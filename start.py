50e371297d89042b21f62f9b02f3e45ecc72a3a8
Jingxin Pan <jingxin.pan@bytedance.com>
2024-10-04 23:03:46 +0000

    add arg parser

--- start.py
+++ start.py
@@ -5,18 +5,13 @@ import fastapi
 from fastapi import FastAPI, Request
 import uvicorn
 import torch
-
-
-
+import argparse
 
 app = FastAPI()
 
 @app.get("/kvmapSize")
 async def read_status():
     return infinity._infinity.get_kvmap_len()
-
-
-
 
 def check_p2p_access():
     num_devices = torch.cuda.device_count()
@@ -30,8 +25,32 @@ def check_p2p_access():
                 else:
                     print(f"Peer access NOT supported between device {i} and {j}")
 
-if __name__ == "__main__":
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host",
+        required=False,
+        help="listen on which host",
+        default="0.0.0.0",
+        type=str,
+    )
+    parser.add_argument(
+        "--port",
+        required=False,
+        default=18080,
+        help="Listen on which port",
+    )
+    parser.add_argument(
+        "--log_level",
+        required=False,
+        default='warning',
+        help="The command to run tests and generate coverage report.",
+        type=str
+    )
+    return parser.parse_args()
 
+if __name__ == "__main__":
+    args = parse_args()
     check_p2p_access()
     infinity.check_infinity_supported()
 
@@ -42,10 +61,10 @@ if __name__ == "__main__":
     infinity.register_server(loop, 16<<30)
     config = uvicorn.Config(
     app,
-    host="0.0.0.0",
-    port=18080,
+    host=args.host,
+    port=args.port,
     loop="uvloop",
-    log_level="warning"  # Disables logging
+    log_level=args.log_level  # Disables logging
     )
 
     server = uvicorn.Server(config)
