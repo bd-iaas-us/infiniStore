7cfc748a3877d1854f2db9e3bb052b4ca8721f5a
dongmao zhang <deanracccoon@gmail.com>
2024-11-12 07:11:37 +0000

    mempool align to 4k

--- src/mempool.cpp
+++ src/mempool.cpp
@@ -20,8 +20,13 @@ MemoryPool::MemoryPool(size_t pool_size, size_t block_size, struct ibv_pd* pd)
         "Memory pool size: {} bytes, block size: {} bytes, total blocks: {}, "
         "it may take a while",
         pool_size_, block_size_, total_blocks_);
-    CHECK_CUDA(cudaMallocHost(&pool_, pool_size_));
-    INFO("Memory pool allocated at {}", pool_);
+    // CHECK_CUDA(cudaMallocHost(&pool_, pool_size_));
+
+    if (posix_memalign(&pool_, 4096, pool_size_) != 0) {
+        ERROR("Failed to allocate memory pool");
+        exit(EXIT_FAILURE);
+    }
+    CHECK_CUDA(cudaHostRegister(pool_, pool_size_, cudaHostRegisterDefault));
 
     // 注册内存区域
     mr_ = ibv_reg_mr(pd_, pool_, pool_size_,
@@ -30,6 +35,8 @@ MemoryPool::MemoryPool(size_t pool_size, size_t block_size, struct ibv_pd* pd)
         ERROR("Failed to register MR");
         exit(EXIT_FAILURE);
     }
+    INFO("Memory pool allocated at {}", pool_);
+
     bitmap_.resize(total_blocks_, 0);
 }
 
